16fad41681215ba205989ee7a5fdeb99dac6f533
CaoKienQuoc <caokienquoc1582002@gmail.com>
2025-04-16 00:18:53 +0700

    update source

--- Services/Service/RequestProductService .cs
+++ Services/Service/RequestProductService .cs
@@ -423,7 +423,7 @@ namespace Services.Service
             if (existingRequest != null)
             {
                 existingOrder = await _orderRepository.GetOrderByRequestIdAsync(existingRequest.RequestProductId);
-                if (existingOrder?.Status == "Paid" || existingOrder?.Status == "Canceled")
+                if (existingOrder?.Status == "Paid" || existingOrder?.Status == "Canceled" || existingRequest.RequestStatus == "Canceled")
                 {
                     existingRequest = null;
                     existingOrder = null;
