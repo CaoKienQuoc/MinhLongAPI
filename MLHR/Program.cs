03810231889efc5c3130b01635b92fa8e8225e4a
CaoKienQuoc <caokienquoc1582002@gmail.com>
2025-04-17 23:07:52 +0700

    update source

--- MLHR/Program.cs
+++ MLHR/Program.cs
@@ -158,6 +158,9 @@ builder.Services.AddScoped<IWarehouseTransferService, WarehouseTransferService>(
 builder.Services.AddScoped<IAgencyLevelRepository, AgencyLevelRepository>();
 builder.Services.AddScoped<IAgencyLevelService, AgencyLevelService>();
 
+builder.Services.AddScoped<IWarehouseExportRepository, WarehouseExportRepository>();
+builder.Services.AddScoped<IWarehouseExportService, WarehouseExportService>();
+
 builder.Services.AddScoped<IPaymentTransactionRepository, PaymentTransactionRepository>();
 builder.Services.AddScoped<IPaymentTransactionService, PaymentTransactionService>();
 builder.Services.AddMemoryCache(); // hoặc services.AddMemoryCache() nếu dùng Startup
