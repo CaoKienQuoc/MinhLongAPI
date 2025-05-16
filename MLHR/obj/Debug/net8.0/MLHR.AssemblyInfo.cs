cc064aeaad56091c7d33e2109098b035d0063777
CaoKienQuoc <caokienquoc1582002@gmail.com>
2025-05-16 23:46:57 +0700

    fix code product

--- MLHR/obj/Debug/net8.0/MLHR.AssemblyInfo.cs
+++ MLHR/obj/Debug/net8.0/MLHR.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MLHR")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf46564b6a13d9a533579186c46ddc3fed6ab3fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53329df8ab3b037a152b64e503a71a6cacb3be56")]
 [assembly: System.Reflection.AssemblyProductAttribute("MLHR")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MLHR")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
