0d417f0edaed6fdee496628ec7a529480e7714b6
ho trung hieu <trunghieuho0909@gmail.com>
2025-05-18 18:56:05 +0700

    update source

--- MLHR/obj/Debug/net8.0/MLHR.AssemblyInfo.cs
+++ MLHR/obj/Debug/net8.0/MLHR.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MLHR")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef7a9f90650329cf1776bb2d765ac16f1ecfe427")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cdbe8f163a86ef735ac52ca748c5e27224d8298")]
 [assembly: System.Reflection.AssemblyProductAttribute("MLHR")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MLHR")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Services/Service/ProductService.cs
+++ Services/Service/ProductService.cs
@@ -56,7 +56,7 @@ namespace Services.Service
                 UpdatedByName = product.Updater?.Employee?.FullName ?? product.Updater?.Username ?? "Chưa cập nhật",
                 UpdatedDate = product.UpdatedDate,
                 AvailableStock = availableStock,
-                Price = (decimal)Math.Ceiling((double)product.Price),
+                Price = product.Price,
                 Images = product.Images.Select(img => img.ImageUrl).ToList()
             };
         }
@@ -87,7 +87,7 @@ namespace Services.Service
                     UpdatedByName = product.Updater?.Employee?.FullName ?? product.Updater?.Username ?? "Chưa cập nhật",
                     UpdatedDate = product.UpdatedDate,
                     AvailableStock = availableStock,
-                    Price = (decimal)Math.Ceiling((double)product.Price),
+                    Price = product.Price,
                     Images = product.Images.Select(img => img.ImageUrl).ToList()
                 });
             }
